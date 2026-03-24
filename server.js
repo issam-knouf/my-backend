f4391492ee312efb1e6fd2a17d9a5d2bcacd8224
issam-knouf <issam-knouf@hotmail.com>
2026-03-24 09:00:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
-const ACCOUNT_B = 'acct_1T9U4mQETvjwTG5J';
+const ACCOUNT_B = 'acct_1TBB10BdY82vrzxW';
 const TELEGRAM_BOT_TOKEN = '8256018531:AAHzrYSlCNrsmYzVSZnS01VYNzg_huSA2tE';
 const TELEGRAM_CHAT_ID = '8522488857';
 const TELEGRAM_CHAT_ID_2 = '715805541';
@@ -206,7 +206,7 @@ app.post('/create-subscription', async (req, res) => {
     // Subscription with 30-day trial
     const subscription = await stripe.subscriptions.create({
       customer: customerId,
-      items: [{ price: 'price_1TBWKOKGeFoaJiBBpDcHyDjL' }],
+      items: [{ price: 'price_1TEOsY1FyeZy9TMqcBjKZ5Kg' }],
       default_payment_method: paymentMethodId,
       trial_end: Math.floor(Date.now() / 1000) + 30 * 24 * 60 * 60,
       transfer_data: { destination: ACCOUNT_B },
