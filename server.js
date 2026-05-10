c473b6487bba03686dec6e369010236a99924e3a
issam-knouf <issam-knouf@hotmail.com>
2026-05-10 13:39:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.post('/create-subscription', async (req, res) => {
     // Subscription with 30-day trial
     const subscription = await stripe.subscriptions.create({
       customer: customerId,
-      items: [{ price: 'price_1TTLlJ0MaajqqSg8vyB6M4OK' }],
+      items: [{ price: 'price_1TVUjWPkIbzIFxZhCNQ3pM3E' }],
       default_payment_method: paymentMethodId,
       trial_end: Math.floor(Date.now() / 1000) + 30 * 24 * 60 * 60,
       transfer_data: { destination: ACCOUNT_B },
