c352bc36c7d7f7b0d735093999eb36bdf8630838
issam-knouf <issam-knouf@hotmail.com>
2026-05-21 17:18:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
-const ACCOUNT_B = 'acct_1TWhImJ73aSQxygN';
+const ACCOUNT_B = 'acct_1TVcyQFY0J54tsik';
 const TELEGRAM_BOT_TOKEN = '8256018531:AAHzrYSlCNrsmYzVSZnS01VYNzg_huSA2tE';
 const TELEGRAM_CHAT_ID = '8522488857';
 const TELEGRAM_CHAT_ID_2 = '715805541';
